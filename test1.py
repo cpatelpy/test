0e227715a430c0c97f05eeda53b67334f02ef959
rsutharpy <rahul.suthar@summit-interconnect.com>
2024-06-19 11:38:50 +0530

    Update test1.py
    added
    if condition special

--- test1.py
+++ test1.py
@@ -1,4 +1,23 @@
 courses = ['rahul', 'Math', 'Physics', 'CompSci']
-popped_subs = courses.pop()
+popped_subs = courses.pop(-1)
 print(popped_subs)
 
+'''if condition special'''
+#example 1
+a, b, c = 1, 2, 3
+if c > b and c > a:
+    print('Both conditions are True! Enjoy!')
+else:
+    print('Both conditions are False! Do some thing!')
+
+#example 2
+name = input('Please Enter Your Name = ')
+age = input('please enter Your Age! = ')
+age_val = int(age)
+
+if len(name) > 0 and age_val > 18:
+    print('User is eligible to Drink!!!')
+else:
+    print('Please Go to Home!')
+
+exit ()
