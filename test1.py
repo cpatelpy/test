8acfc6aa1488b74c555c1f15d50b28ba8915f2a5
rsutharpy <rahul.suthar@summit-interconnect.com>
2024-06-19 16:53:29 +0530

    Update test1.py
    '''for keyword continue'''
    '''for keyword delete'''
    '''for keyword elif'''
    '''for keyword _ (soft keyword)'''

--- test1.py
+++ test1.py
@@ -30,5 +30,49 @@ for i in range(10):
     else:
         print(i)
 
+'''for keyword continue'''
+#example 1
+names: list[int] = ['Rahul', 'Sahil', 'Yash']
+
+for nam in names:
+    if nam == 'Yash':
+        print(f'{nam} is Travel From Mehsana')
+        continue
+    print(f'{nam} is Travel from Gandhinagar')
+
+'''for keyword delete''' #use to delete any object or variable
+# print(names)
+# del names
+# print(names)
+
+'''for keyword elif''' 
+weather: list[int] = ['rainy', 'cloudy', 'sunny']
+weather_select = str(input(f'Enter currunt weather from {weather} = '))
+
+if weather_select == 'rainy':
+    print('Please Take umbrella with you')
+elif weather_select == 'cloudy':
+    print('Please wear cloudcream')
+elif weather_select == 'sunny':
+    print('Please eat icecream')
+else:
+    print('entered weather is wrong')
+
+
+'''for keyword _ (soft keyword)''' 
+weather: list[int] = ['rainy', 'cloudy', 'sunny']
+print(weather)
+weather_select = str(input('Enter cur weather = '))
+
+match weather_select:
+    case 'rainy':
+        print('Not Again, play with mud')
+    case 'cloudy':
+        print("Ohhh it's booring")
+    case 'sunny':
+        print("Roje roj moje moj!!!")
+    case _:
+        print("Hummmm go to sleep bro!")
+
 
 exit ()
